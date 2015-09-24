3201d1567905f3ea618d8bb6e6dee0ea6f1bc8b3
Dustin Specker <dustinspecker@dustinspecker.com>
2015-09-24 17:52:16 -0500

    feat(app): add users route

---
+++ app/routes/users.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return [
+      {
+        name: "Brent Schooley",
+        twitter: "@brentschooley"
+      },
+      {
+        name: "Sam Agnew",
+        twitter: "@sagnewshreds"
+      },
+      {
+        name: "Eddie Zaneski",
+        twitter: "@eddiezane"
+      }
+    ];
+  }
+});
